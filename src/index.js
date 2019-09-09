99fd5800296650e7cafd88995a96aac95cda57c6
jrfaber <jrfaber@valdosta.edu>
2019-09-09 17:34:54 -0400

    Populated list of video items

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import SearchBar from "./components/search_bar";
 import YTSearch from 'youtube-api-search';
 import VideoList from "./components/video_list";
+import VideoDetail from "./components/video_detail";
+
 
 const API_KEY = 'AIzaSyCLbKOa_gXSZ7t15gLv4j56XzxiUSpompI';
 
@@ -20,6 +22,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
                 <VideoList videos={this.state.videos} />
             </div>
         );
