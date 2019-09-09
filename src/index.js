5bb56428ba73037e06182a0c291b99403201a7ea
jrfaber <jrfaber@valdosta.edu>
2019-09-09 17:08:06 -0400

    created a React 'class component'

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SearchBar from "./components/search_bar";
+
+const API_KEY = 'AIzaSyCLbKOa_gXSZ7t15gLv4j56XzxiUSpompI';
+
+const App = () => {
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
